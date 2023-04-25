cdaf759e1883d65f82d676af1cf0671eb0b7d7bf
nemlic <nemlicmain22@gmail.com>
2023-04-25 17:14:37 +0000

    modify specifiers

--- specifiers.c
+++ specifiers.c
@@ -11,7 +11,9 @@ int (*get_spec_func(char specifier))(va_list)
 {
 	conversion_t specifiers[] = {
 		{'s', print_string}, {'c', print_char}, {'%', print_percent},
-		{'i', print_integer}, {'d', print_integer}, {0, NULL}
+		{'i', print_integer}, {'d', print_integer},
+		{'b', print_binary}, {'u', print_unsigned}, {'o', print_octal},
+		{0, NULL}
 	};
 	int i = 0;
 
